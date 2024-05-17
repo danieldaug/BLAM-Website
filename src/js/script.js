d565729edc6747a6c2c4ddc568b6d7d5299f851b
danieldaug <danieldaug3@gmail.com>
2024-05-16 17:51:57 -0700

    added gallery content and randomization of layout

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,16 @@ function loadnav(b){
 
 var bar = document.getElementById("navbar")
 loadnav(bar);
+
+//function to randomize sizes of gallery images
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll('.artpiece');
+
+  elements.forEach(element => {
+      // Generate random width and height between 300px and 550px
+      const randomWidth = Math.floor(Math.random() * 250) + 300;
+
+      // Apply random sizes to the element
+      element.style.width = randomWidth + 'px';
+  });
+});
